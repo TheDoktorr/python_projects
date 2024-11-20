5903f88ae517e885901d9981dc644556432c1b3b
Andrew Hamill <andrewhamill8@gmail.com>
2024-11-20 22:34:47 +0000

    added ext

--- 281/weekly exercises/week06/Particle.py
+++ 281/weekly exercises/week06/Particle.py
@@ -7,13 +7,15 @@ class Particle:
         velocity=np.array([0, 0, 0], dtype=float),
         acceleration=np.array([0, -10, 0], dtype=float),
         name='Ball',
-        mass=1.0
+        mass=1.0,
+        G = 6.67408E-11
     ):
         self.position = np.copy(position).astype(float)
         self.velocity = np.copy(velocity).astype(float)
         self.acceleration = np.copy(acceleration).astype(float)
         self.name = np.copy(name)
         self.mass = np.copy(mass)
+        self.G = G
         
         
     def __str__(self):
@@ -25,17 +27,21 @@ class Particle:
         self.deltaT = deltaT
         
         # define start variables 
-        time = 0 
-        r0 = self.position
-        v0 = self.velocity
-        a0 = self.acceleration
-
-
-        self.position = r0 + v0 * self.deltaT
-        self.velocity = v0  + a0 * self.deltaT
+        self.position = self.position + self.velocity * self.deltaT
+        self.velocity = self.velocity  + self.acceleration * self.deltaT
 
         return self.position, self.velocity
     
+    def updateGravitationalAcceleration(self, body):
+        # define Gravitational constant
+  
+        
 
+        gravaccel = - (self.G * body.mass) * (body.position - self.position) / (np.linalg.norm(body.position - self.position ))**3
+        return gravaccel
+    
+
+
+# test code
 
             
---
+++ 281/weekly exercises/week06/Simulation.py
@@ -0,0 +1,47 @@
+import numpy as np
+from Particle import Particle
+
+earthMass = 5.97237e24     # https://en.wikipedia.org/wiki/Earth
+earthRadius = 63710 * 1e3  # https://en.wikipedia.org/wiki/Earth
+Earth = Particle(
+    position=np.array([0, 0, 0]),
+    velocity=np.array([0, 0, 0]),
+    acceleration=np.array([0, 0, 0]),
+    name="Earth",
+    mass=earthMass
+)
+satPosition = earthRadius + (35786 * 1e3)
+satVelocity = np.sqrt(Earth.G * Earth.mass / satPosition)  # from centrifugal force = gravitational force
+Satellite = Particle(
+    position=np.array([satPosition, 0, 0]),
+    velocity=np.array([0, satVelocity, 0]),
+    acceleration=np.array([0, 0, 0]),
+    name="Satellite",
+    mass=100.
+)
+
+
+
+
+from Simulation import *
+print("The Earth and Satellite's locations after {0} seconds are:".format((2000*6)))
+for particle in [Earth, Satellite]:
+    print("  Particle: {}".format(particle.name))
+    print("    Mass: {0:.3e}, ".format(particle.mass))
+    for attribute in ["position", "velocity", "acceleration"]:
+        print("    {}: {}".format(attribute, particle.__getattribute__(attribute) + 0.0))  # add 0.0 to avoid negative zeros!
+
+
+iterations = 2000
+time = 0 
+deltaT = 6 
+while time <= ( deltaT * iterations):
+    for particle in [Earth, Satellite]:
+        particle.updateGravitationalAcceleration(particle)
+        particle.update(deltaT)
+        time += deltaT
+
+        # if first particle =x second particle 
+        
+        
+
